58a849b6a70d3b34f7c080355d50a0b866821772
anandrajB <anand98.ar@gmail.com>
2021-11-24 15:13:06 +0530

    heroku new

--- AdminTask/settings.py
+++ AdminTask/settings.py
@@ -150,7 +150,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 AUTHENTICATION_BACKENDS = [
